0fd74f0b3dde765671612aaaf39ada859cb7be67
zwhstn-boop <zwhstn@gmail.com>
2026-05-04 10:48:20 +0900

    test change

---
+++ src/main/java/com/back/SpringTesstApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTesstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTesstApplication.class, args);
+    }
+
+}
